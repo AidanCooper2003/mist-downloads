c80d0293fc85e774ecc41a9cbfb09df5e78c3c90
Aidan Cooper <aidcoope@iu.edu>
2023-04-08 14:40:56 -0400

    Spring Initializer Initial Commit

---
+++ src/main/java/edu/iu/c322/mist/downloads/DownloadsApplication.java
@@ -0,0 +1,13 @@
+package edu.iu.c322.mist.downloads;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DownloadsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DownloadsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/iu/c322/mist/downloads/DownloadsApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.iu.c322.mist.downloads;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DownloadsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
